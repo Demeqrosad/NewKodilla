967adcb2fa141613ce01f502d31f4d15ac39e39e
Demeqrosad <demetriusz@kurosad.pl>
2020-01-30 19:59:49 +0100

    Module 20.4 update

---
+++ src/main/java/com/newkodilla/patterns2/observer/forum/JavaToolsForumTopic.java
@@ -0,0 +1,9 @@
+package com.newkodilla.patterns2.observer.forum;
+
+public class JavaToolsForumTopic extends ForumTopic
+{
+    public JavaToolsForumTopic()
+    {
+        super("Java Tools Group");
+    }
+}
