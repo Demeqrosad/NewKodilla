b51ec679678f520b67193470f766be885f1d8df1
Demeqrosad <demetriusz@kurosad.pl>
2020-02-01 21:40:38 +0100

    Module 20.5 Statistics added

--- src/main/java/com/newkodilla/patterns2/adapter/bookclassifier/MedianAdaptee.java
+++ src/main/java/com/newkodilla/patterns2/adapter/bookclassifier/MedianAdaptee.java
@@ -3,49 +3,23 @@ package com.newkodilla.patterns2.adapter.bookclassifier;
 import com.newkodilla.patterns2.adapter.bookclassifier.libraryb.Book;
 import com.newkodilla.patterns2.adapter.bookclassifier.libraryb.BookSignature;
 import com.newkodilla.patterns2.adapter.bookclassifier.libraryb.BookStatistics;
+import com.newkodilla.patterns2.adapter.bookclassifier.libraryb.Statistics;
 
-import java.util.Arrays;
 import java.util.Map;
 
 public class MedianAdaptee implements BookStatistics
 {
+    Statistics statistics = new Statistics();
+
     @Override
     public int averagePublicationYear(Map<BookSignature, Book> books)
     {
-        if(books.size() == 0)
-        {
-            return 0;
-        }
-        int sum = 0;
-        for(Map.Entry<BookSignature, Book> entry : books.entrySet())
-        {
-            sum += entry.getValue().getYearOfPublication();
-        }
-        return sum / books.size();
+        return statistics.averagePublicationYear(books);
     }
 
     @Override
     public int medianPublicationYear(Map<BookSignature, Book> books)
     {
-        if(books.size() == 0)
-        {
-            return 0;
-        }
-        int[] years = new int[books.size()];
-        int n = 0;
-        for(Map.Entry<BookSignature, Book> entry : books.entrySet())
-        {
-            years[n] = entry.getValue().getYearOfPublication();
-            n++;
-        }
-        Arrays.sort(years);
-        if (years.length % 2 == 0)
-        {
-            return years[(int)(years.length / 2 + 0.5)];
-        }
-        else
-        {
-            return years[years.length / 2];
-        }
+        return statistics.medianPublicationYear(books);
     }
 }
